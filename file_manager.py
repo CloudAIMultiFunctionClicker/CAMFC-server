410ed3642f16f4bd44857857ce709c22e9d513ca
ant-cave <ANTmmmmm@126.com>
2026-01-06 18:08:19 +0800

    build: 将许可证从 GPLv3 更改为 AGPLv3
    - 更新了 LICENCE 文件，将 GNU 通用公共许可证 (GPLv3) 替换为 GNU Affero 通用公共许可证 (AGPLv3)
    - 更新了所有源文件中的版权声明，添加了中文名和新的许可证信息
    - 修改了 AGPLv3 特定的条款，包括网络交互和源码提供要求
    - 更新了版权年份和作者信息

--- file_manager.py
+++ file_manager.py
@@ -1,21 +1,24 @@
 """
-Copyright (C) 2026 Jiale Xu (ANTmmmmm) <https://github.com/ant-cave>
+Copyright (C) 2026 Jiale Xu (许嘉乐) (ANTmmmmm) <https://github.com/ant-cave>
 Email: ANTmmmmm@outlook.com, ANTmmmmm@126.com, 1504596931@qq.com
 
-Copyright (C) 2026 Xinhang Chen <https://github.com/cxh09>
+Copyright (C) 2026 Xinhang Chen (陈欣航) <https://github.com/cxh09>
 Email: abc.cxh2009@foxmail.com
 
+Copyright (C) 2026 Zimo Wen (温子墨) <https://github.com/lusamaqq>
+Email: 1220594170@qq.com
+
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 文件管理模块
